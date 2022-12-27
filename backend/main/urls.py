ed829ced14bd84ada9dbb6daddec43cdcd68f941
thea-exe <csokkunthea@gmail.com>
2022-12-27 14:07:00 -0500

    sqlite

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("differentView/", views.differentView, name="differentView")
+    path("<int:id>", views.index, name="index"),
 ]
