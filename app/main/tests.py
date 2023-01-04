783dcd5f846be15af758ea2898efb47fc61bbefe
Xiangan He <xiangan9@gmail.com>
2023-01-04 11:39:03 -0500

    initial test cases

--- app/main/tests.py
+++ app/main/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import ToDoList, Item
 
 # Create your tests here.
+
+class ToDoListTest(TestCase):
+    def setUp(self):
+        testlist = ToDoList.objects.get(name="test")
+        self.assertEqual(testlist.name, "test")
+        
+class ItemTest(TestCase):
+    def setUp(self):
+        todolist = ToDoList.objects.get(name="test")
+        testitem = Item.objects.get(todolist, text="testitem")
+        self.assertEqual(testitem.todolist.name, "test");
+        self.assertEqual(testitem.name, "testitem")
