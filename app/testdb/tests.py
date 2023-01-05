d987b96d6e7710ea5ff41d7893b9f8d0c1e76555
thea-exe <csokkunthea@gmail.com>
2023-01-05 11:35:52 -0500

    tests.py

--- app/testdb/tests.py
+++ app/testdb/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
-
+from .models import Teacher
 # Create your tests here.
+
+
+# Adding an object and accessing it
+class TeacherTest(TestCase):
+    def setUp(self):
+        Teacher.objects.create(name="test", age=20)
+
+    def test_object_name(self):
+        teachertest = Teacher.objects.get(name="test", age=20)
+        self.assertEqual(teachertest.name, "test")
+        self.assertEqual(teachertest.age, 20)
